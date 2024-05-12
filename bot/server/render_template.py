2281809d61f562e252e1d3d1abfff89f24b1ce2d
Jisshu developer <142872501+Jisshubot@users.noreply.github.com>
2024-05-12 17:30:44 +0530

    Update render_template.py

--- bot/server/render_template.py
+++ bot/server/render_template.py
@@ -2,7 +2,7 @@ from aiofiles import open as aiopen
 from os import path as ospath
 
 from bot import LOGGER
-from bot.config import Telegram
+from bot.jisshu import Telegramm
 from bot.helper.exceptions import InvalidHash
 from bot.helper.utils import get_readable_file_size
 from bot.server.file_properties import get_file_ids
@@ -22,20 +22,20 @@ async def render_page(id, secure_hash, is_admin=False, html='', playlist='', dat
     tpath = ospath.join('bot', 'server', 'template')
     if route == 'login':
         async with aiopen(ospath.join(tpath, 'login.html'), 'r') as f:
-            html = (await f.read()).replace("<!-- Error -->", msg or '').replace("<!-- Theme -->", Telegram.THEME).replace("<!-- RedirectURL -->", redirect_url)
+            html = (await f.read()).replace("<!-- Error -->", msg or '').replace("<!-- Theme -->", Telegramm.THEME).replace("<!-- RedirectURL -->", redirect_url)
     elif route == 'home':
         async with aiopen(ospath.join(tpath, 'home.html'), 'r') as f:
-            html = (await f.read()).replace("<!-- Print -->", html).replace("<!-- Theme -->", Telegram.THEME).replace("<!-- Playlist -->", playlist)
+            html = (await f.read()).replace("<!-- Print -->", html).replace("<!-- Theme -->", Telegramm.THEME).replace("<!-- Playlist -->", playlist)
             if not is_admin:
                 html += admin_block
     elif route == 'playlist':
         async with aiopen(ospath.join(tpath, 'playlist.html'), 'r') as f:
-            html = (await f.read()).replace("<!-- Theme -->", Telegram.THEME).replace("<!-- Playlist -->", playlist).replace("<!-- Database -->", database).replace("<!-- Title -->", msg).replace("<!-- Parent_id -->", id)
+            html = (await f.read()).replace("<!-- Theme -->", Telegramm.THEME).replace("<!-- Playlist -->", playlist).replace("<!-- Database -->", database).replace("<!-- Title -->", msg).replace("<!-- Parent_id -->", id)
             if not is_admin:
                 html += admin_block
     elif route == 'index':
         async with aiopen(ospath.join(tpath, 'index.html'), 'r') as f:
-            html = (await f.read()).replace("<!-- Print -->", html).replace("<!-- Theme -->", Telegram.THEME).replace("<!-- Title -->", msg).replace("<!-- Chat_id -->", chat_id)
+            html = (await f.read()).replace("<!-- Print -->", html).replace("<!-- Theme -->", Telegramm.THEME).replace("<!-- Title -->", msg).replace("<!-- Chat_id -->", chat_id)
             if not is_admin:
                 html += admin_block
     else:
@@ -50,8 +50,8 @@ async def render_page(id, secure_hash, is_admin=False, html='', playlist='', dat
         if tag == 'video':
             async with aiopen(ospath.join(tpath, 'video.html')) as r:
                 poster = f"/api/thumb/{chat_id}?id={id}"
-                html = (await r.read()).replace('<!-- Filename -->', filename).replace("<!-- Theme -->", Telegram.THEME).replace('<!-- Poster -->', poster).replace('<!-- Size -->', size).replace('<!-- Username -->', StreamBot.me.username)
+                html = (await r.read()).replace('<!-- Filename -->', filename).replace("<!-- Theme -->", Telegramm.THEME).replace('<!-- Poster -->', poster).replace('<!-- Size -->', size).replace('<!-- Username -->', StreamBot.me.username)
         else:
             async with aiopen(ospath.join(tpath, 'dl.html')) as r:
-                html = (await r.read()).replace('<!-- Filename -->', filename).replace("<!-- Theme -->", Telegram.THEME).replace('<!-- Size -->', size)
+                html = (await r.read()).replace('<!-- Filename -->', filename).replace("<!-- Theme -->", Telegramm.THEME).replace('<!-- Size -->', size)
     return html
