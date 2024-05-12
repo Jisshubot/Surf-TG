7e35fb5de65f8a83a9e0998ea255f0539cee586f
Jisshu developer <142872501+Jisshubot@users.noreply.github.com>
2024-05-12 17:27:24 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,13 +11,6 @@ class Telegram:
     PORT = int(getenv("PORT", "8080"))
     BASE_URL = getenv("BASE_URL").rstrip('/')
     DATABASE_URL = getenv("DATABASE_URL")
-    AUTH_CHANNEL = [channel.strip()
-                    for channel in getenv("AUTH_CHANNEL").split(",")]
-    THEME = getenv("THEME", "quartz")
-    USERNAME = getenv("USERNAME", "admin")
-    PASSWORD = getenv("PASSWORD", "admin")
-    ADMIN_USERNAME = getenv("ADMIN_USERNAME", "surfTG")
-    ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "surfTG")
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '10'))
     MULTI_CLIENT = bool(getenv('MULTI_CLIENT', 'False'))
