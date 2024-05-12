19622f6ed276782cd52b2e95807d317ea7ab98c2
Jisshu developer <142872501+Jisshubot@users.noreply.github.com>
2024-05-12 17:26:11 +0530

    Update jisshu.py

--- bot/jisshu.py
+++ bot/jisshu.py
@@ -1,1 +1,9 @@
+from os import getenv
 
+
+AUTH_CHANNEL = [channel.strip()for channel in getenv("AUTH_CHANNEL").split(",")]
+THEME = getenv("THEME", "quartz")
+USERNAME = getenv("USERNAME", "admin")
+PASSWORD = getenv("PASSWORD", "admin")
+ADMIN_USERNAME = getenv("ADMIN_USERNAME", "surfTG")
+ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "surfTG")
