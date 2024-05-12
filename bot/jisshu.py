a84d5f657da8e65e850092653f90047c34dc572f
Jisshu developer <142872501+Jisshubot@users.noreply.github.com>
2024-05-12 18:08:46 +0530

    Update jisshu.py

--- bot/jisshu.py
+++ bot/jisshu.py
@@ -1,8 +1,8 @@
 from os import getenv
 
 class Telegramm:
-THEME = getenv("THEME", "quartz")
-USERNAME = getenv("USERNAME", "jisshu")
-PASSWORD = getenv("PASSWORD", "jisshu")
-ADMIN_USERNAME = getenv("ADMIN_USERNAME", "zishan")
-ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "zkhan111@")
+    THEME = getenv("THEME", "quartz")
+    USERNAME = getenv("USERNAME", "jisshu")
+    PASSWORD = getenv("PASSWORD", "jisshu")
+    ADMIN_USERNAME = getenv("ADMIN_USERNAME", "zishan")
+    ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "zkhan111@")
